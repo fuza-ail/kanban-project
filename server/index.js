258968b0c28df380f9d3f9ea8f1e093b104ba108
Muhammad Fuzail <mfuzail1991@gmail.com>
2022-01-19 00:35:43 +0700

    port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const router = require("./router");
 const errorHandler = require("./middlewares/errorHandler");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
