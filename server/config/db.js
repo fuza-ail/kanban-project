9a2787e083db4c929257b5c1f441a126aef5007d
Muhammad Fuzail <mfuzail1991@gmail.com>
2022-01-19 00:22:31 +0700

    db setup

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  password: "rahasia",
-  database: "kanban",
-  port: 5432
+  user: process.env.USER,
+  host: process.env.HOST,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
 });
 
 module.exports = pool;
